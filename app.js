b0b9ef33a70c4bfc6fb78d09a6eca71f4a678c61
arlei <arleidt@yahoo.com.br>
2020-10-26 01:15:06 -0300

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+var calculadora = require("./calculadora");
+
+console.log(calculadora.nome);
+console.log(calculadora.soma(10,20));
+var resultado = (calculadora.mult(20,10));
+var resultsub = (calculadora.sub(40,20));
+console.log(calculadora.div(10,20));
+console.log(resultado);
+console.log(resultsub);
+console.log(calculadora.soma(10,30));
+
+
+
+
+
+/*var mostrarSite = true;
+const site = "www.google.com.br"
+console.log("Hello World!");
+console.log("Meu nome é Arlei");
+console.log("E estou estudando Node.js");
+
+if(mostrarSite){
+  console.log(site);
+}**/
