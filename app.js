92e652ae1b727027491890ef92562de314f0b2a7
arlei duarte <arleidt@yahoo.com.br>
2020-10-31 17:59:16 -0300

    hotfix

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var calculadora = require("./calculadora");
 
 console.log(calculadora.nome);
@@ -22,3 +23,16 @@ console.log("E estou estudando Node.js");
 if(mostrarSite){
   console.log(site);
 }**/
+=======
+//chamando o módulo http do núcleo do node e passando para a var http
+//localhost:8181 nada acontece precisa passar alguma coisa function req res
+//http requisição e o server da uma resposta
+
+var http = require("http");
+
+http.createServer(function(requisicao, resposta) {
+    resposta.end("<h1>Bem vindo ao meu Site</h1><h4>google.com.br</h4>")
+
+}).listen(8181);
+    console.log("Meu primeiro servidor rodando");
+>>>>>>> 8f91f4c... create http
