8f91f4ced8aaecd76e14e9435db51b49d208a1c1
arlei duarte <arleidt@yahoo.com.br>
2020-10-31 17:48:40 -0300

    create http

---
+++ app.js
@@ -0,0 +1,11 @@
+//chamando o módulo http do núcleo do node e passando para a var http
+//localhost:8181 nada acontece precisa passar alguma coisa function req res
+//http requisição e o server da uma resposta
+
+var http = require("http");
+
+http.createServer(function(requisicao, resposta) {
+    resposta.end("<h1>Bem vindo ao meu Site</h1><h4>google.com.br</h4>")
+
+}).listen(8181);
+    console.log("Meu primeiro servidor rodando");
