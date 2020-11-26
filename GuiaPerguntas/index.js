5b3f8b103a831301e7970222511ae44249f7966e
arlei duarte <arleidt@yahoo.com.br>
2020-11-26 04:29:00 -0300

    forEach produtos

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -7,14 +7,27 @@ const app = express(); //recebe express na var app
  */
 app.set("view engine", "ejs");
 
-app.get("/:nome", function(req, res){//criando rota com resposta
+app.get("/:nome/:lang", function(req, res){//criando rota com resposta
   var nome = req.params["nome"];
-  var lang = "JavaScript";
-  res.render("index.ejs", {
+  var lang = req.params.lang;
+  var exibirMsg = false;
+
+  var produtos = [ //array de obj produtos
+    {nome : "Doritos", preco : 3.14},
+    {nome : "Coca Cola", preco : 5},
+    {nome : "Leite", preco : 1.45},
+    {nome : "Carne", preco : 20},
+    {nome : "RedBull", preco : 6},
+    {nome : "Nescau", preco : 4}
+  ]
+
+  res.render("index.ejs", { //obj
     nome : nome,//Passando valores para html
     lang : lang,
     idade : 31,
-    empresa : "Compasso UOL"
+    empresa : "Compasso UOL",
+    msg: exibirMsg,
+    produtos : produtos
   })//vai desenhar arquivo index.ejs render olha automaticamente na pasta views
 });
 
